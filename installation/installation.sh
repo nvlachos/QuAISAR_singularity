a4d6b536e05d1fdff01fc10f9417b540ab88ec69
Nick <nvx4@cdc.gov>
2021-08-16 12:30:00 -0400

    uncommented to test full installation

--- installation/installation.sh
+++ installation/installation.sh
@@ -76,101 +76,101 @@ elif [ -z "${working_directory}" ]; then
 fi
 
 
-# echo -e "${installation_location}\n${databases}\n${working_directory}"
-#
-# echo "Installing quaisar scripts to ${installation_location}"
-# if [[ ! -d ${installation_location} ]]; then
-#   echo "Creating ${installation_location}"
-#   mkdir -p ${installation_location}
-# fi
-#
-# function test_go() {
-#   current_dir=$(pwd)
-#   echo "Testing Go!"
-#   echo -e "package main\n" > ${installation_location}/hello.go
-#   echo -e "import \"fmt\"\n" >> ${installation_location}/hello.go
-#   echo -e "func main() {\n\tfmt.Printf(\"hello, world\")\n}" >> ${installation_location}/hello.go
-# 	cd ${installation_location}
-# 	go build hello.go
-# 	sleep 2
-#   go_test_output=$(./hello)
-#   if [[ "${go_test_output}" = "hello, world" ]]; then
-#     echo "Go is installed correctly, proceed!"
-#   else
-#     echo "Go is not installed, halting"
-#     exit 99
-#   fi
-#   cd ${current_dir}
+echo -e "${installation_location}\n${databases}\n${working_directory}"
+
+echo "Installing quaisar scripts to ${installation_location}"
+if [[ ! -d ${installation_location} ]]; then
+  echo "Creating ${installation_location}"
+  mkdir -p ${installation_location}
+fi
+
+function test_go() {
+  current_dir=$(pwd)
+  echo "Testing Go!"
+  echo -e "package main\n" > ${installation_location}/hello.go
+  echo -e "import \"fmt\"\n" >> ${installation_location}/hello.go
+  echo -e "func main() {\n\tfmt.Printf(\"hello, world\")\n}" >> ${installation_location}/hello.go
+	cd ${installation_location}
+	go build hello.go
+	sleep 2
+  go_test_output=$(./hello)
+  if [[ "${go_test_output}" = "hello, world" ]]; then
+    echo "Go is installed correctly, proceed!"
+  else
+    echo "Go is not installed, halting"
+    exit 99
+  fi
+  cd ${current_dir}
+}
+
+function test_singularity() {
+  sing_version=$(singularity --version)
+  if [[ "${sing_version}" = "singularity version"* ]]; then
+    v3plus=$(echo "${sing_version}" | cut -d' ' -f3 | cut -d'.' -f1)
+    echo "${v3plus}"
+    if [[ "${v3plus}" -ge 3 ]]; then
+      echo "Singularity installed and ready to go"
+    else
+      echo "Your singularity version is too old, please upgrade to version 3 (or remove old versions and change parameters) before trying again"
+      exit 100
+    fi
+  else
+    echo "You do not have singularity installed, either choose a supported operating system, or preinstall before trying to install QuAISAR"
+    exit 101
+  fi
+}
+
+# function test_curl() {
+# 	curled=$(curl -V)
+# 	if [[ "${curled}" = "curl 7."* ]]; then
+# 		echo "curl version 7.X installed, good to go"
+# 	else
+# 		echo "curl is NOT installed and is needed to download some databases, please install it using 'sudo apt get install curl' or 'sudo yum install curl'"
+# 		exit 102
+# 	fi
 # }
-#
-# function test_singularity() {
-#   sing_version=$(singularity --version)
-#   if [[ "${sing_version}" = "singularity version"* ]]; then
-#     v3plus=$(echo "${sing_version}" | cut -d' ' -f3 | cut -d'.' -f1)
-#     echo "${v3plus}"
-#     if [[ "${v3plus}" -ge 3 ]]; then
-#       echo "Singularity installed and ready to go"
-#     else
-#       echo "Your singularity version is too old, please upgrade to version 3 (or remove old versions and change parameters) before trying again"
-#       exit 100
-#     fi
-#   else
-#     echo "You do not have singularity installed, either choose a supported operating system, or preinstall before trying to install QuAISAR"
-#     exit 101
-#   fi
-# }
-#
-# # function test_curl() {
-# # 	curled=$(curl -V)
-# # 	if [[ "${curled}" = "curl 7."* ]]; then
-# # 		echo "curl version 7.X installed, good to go"
-# # 	else
-# # 		echo "curl is NOT installed and is needed to download some databases, please install it using 'sudo apt get install curl' or 'sudo yum install curl'"
-# # 		exit 102
-# # 	fi
-# # }
-#
-# # Do checks of singularity and Go to ensure they are installed already
-# test_go
-# test_singularity
-#
-# echo "Go and Singularity are installed...proceed with installation"
-#
-# echo "Checking for working directory of runs ${working_directory}"
-# if [[ ! -d ${working_directory} ]]; then
-#   echo "Creating ${working_directory}"
-#   mkdir -p ${working_directory}
-# fi
-#
-# # Create primary config file
-# head -n25 ${install_script_dir}/installation/config_template.sh > ${installation_location}/new_config.sh
-# echo "output_dir=${working_directory}" >> ${installation_location}/new_config.sh
-# head -n27 ${install_script_dir}/installation/config_template.sh | tail -n1 >> ${installation_location}/new_config.sh
-# echo "src=${installation_location}" >> ${installation_location}/new_config.sh
-# head -n29 ${install_script_dir}/installation/config_template.sh | tail -n1 >> ${installation_location}/new_config.sh
-# echo "local_DBs=${databases}" >> ${installation_location}/new_config.sh
-# head -n31 ${install_script_dir}/installation/config_template.sh | tail -n1 >> ${installation_location}/new_config.sh
-# CPUs=$(nproc --all)
-# echo "procs=${CPUs}" >> ${installation_location}/new_config.sh
-# tail -n91 ${install_script_dir}/installation/config_template.sh >> ${installation_location}/new_config.sh
-# echo "source ${HOME}/miniconda3/etc/profile.d/conda.sh" >> ${installation_location}/new_config.sh
-#
-# # Copy all scripts from this folder to install location
-# cp ${install_script_dir}/scripts/* ${installation_location}
-# rm ${installation_location}/config.sh
-# mv ${installation_location}/new_config.sh ${installation_location}/config.sh
-# echo "${installation_location}"
-# chmod +x ${installation_location}/*.sh ${installation_location}/*.py
-#
-#
-# # Install databases
-# #Create database folder
-# if [[ ! -d ${databases} ]]; then
-#   echo "Creating ${databases} for databases"
-#   mkdir -p "${databases}/BUSCO"
-# fi
-# echo "${install_script_dir}/scripts/database_checker.sh ${databases} -i"
-# ${install_script_dir}/scripts/database_checker.sh ${databases} -i
+
+# Do checks of singularity and Go to ensure they are installed already
+test_go
+test_singularity
+
+echo "Go and Singularity are installed...proceed with installation"
+
+echo "Checking for working directory of runs ${working_directory}"
+if [[ ! -d ${working_directory} ]]; then
+  echo "Creating ${working_directory}"
+  mkdir -p ${working_directory}
+fi
+
+# Create primary config file
+head -n25 ${install_script_dir}/installation/config_template.sh > ${installation_location}/new_config.sh
+echo "output_dir=${working_directory}" >> ${installation_location}/new_config.sh
+head -n27 ${install_script_dir}/installation/config_template.sh | tail -n1 >> ${installation_location}/new_config.sh
+echo "src=${installation_location}" >> ${installation_location}/new_config.sh
+head -n29 ${install_script_dir}/installation/config_template.sh | tail -n1 >> ${installation_location}/new_config.sh
+echo "local_DBs=${databases}" >> ${installation_location}/new_config.sh
+head -n31 ${install_script_dir}/installation/config_template.sh | tail -n1 >> ${installation_location}/new_config.sh
+CPUs=$(nproc --all)
+echo "procs=${CPUs}" >> ${installation_location}/new_config.sh
+tail -n91 ${install_script_dir}/installation/config_template.sh >> ${installation_location}/new_config.sh
+echo "source ${HOME}/miniconda3/etc/profile.d/conda.sh" >> ${installation_location}/new_config.sh
+
+# Copy all scripts from this folder to install location
+cp ${install_script_dir}/scripts/* ${installation_location}
+rm ${installation_location}/config.sh
+mv ${installation_location}/new_config.sh ${installation_location}/config.sh
+echo "${installation_location}"
+chmod +x ${installation_location}/*.sh ${installation_location}/*.py
+
+
+# Install databases
+#Create database folder
+if [[ ! -d ${databases} ]]; then
+  echo "Creating ${databases} for databases"
+  mkdir -p "${databases}/BUSCO"
+fi
+echo "${install_script_dir}/scripts/database_checker.sh ${databases} -i"
+${install_script_dir}/scripts/database_checker.sh ${databases} -i
 
 
 #check if conda is installed already
